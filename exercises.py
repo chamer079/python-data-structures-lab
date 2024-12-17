d19f4a2a6f14d09da288ca5c5f07097671c63e1b
chamer79 <chamer79@gmail.com>
2024-12-16 21:16:50 -0500

    working on exercise 5

--- exercises.py
+++ exercises.py
@@ -100,3 +100,21 @@ def hometown_info():
 
 # Call the function and print the result
 print('Exercise 4:', hometown_info())
+
+
+# Exercise 5: Iterating Over Dictionary Items
+#
+# Define an empty list named home_town_items.
+# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
+
+def list_home_town_items():
+    # your code here
+    home_town_items = {
+        "city": "",
+        "state": "",
+        "population": ""
+    }
+
+# Call the function and print the result
+print('Exercise 5:', list_home_town_items())
+
